b5a1d8885ffa51c87ab32be7a2039f5cb0990045
darurats <developer.iman@gmail.com>
2018-01-06 10:57:49 +0700

    Update cors

--- app.js
+++ app.js
@@ -40,6 +40,10 @@ app.use('/', index);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if(req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Method', 'PUT,POST,DELETE');
+    return res.status(200).json({});
+  }
   next();
 });
 
